3a537d5f2988fe0617268256ff38151edc2c8f7e
Vince <vincentfiorilli@gmail.com>
2018-06-14 21:11:57 -0400

    add modal, responsive voice

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+//chuck norris ajax request working
+let queryURL = "https://api.chucknorris.io/jokes/random"
 
+$(document).ready(function(){
+  $('#smileButton').on('click', function(event){
+    event.preventDefault()
+
+    $.ajax({
+      url: queryURL,
+      method: 'GET'
+    }).done(function(response){
+      //console.log(response.value)
+      responsiveVoice.speak(response.value)
+      $('#joke').html(response.value)
+    })
+  })
+
+
+
+})
